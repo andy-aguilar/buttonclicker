c49ec89921336f1e2730e0da769c512c8ead7a31
JuniperRhy <gwyniver.rhy@gmail.com>
2021-06-22 13:54:35 -0500

    updated alert

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-document.addEventListener("DOMContentLoaded", ()=> {
-    const button = document.querySelector("#button")
-    button.addEventListener("click", ()=> {
-        alert("Hi")
-    })
-})
+document.addEventListener("DOMContentLoaded", () => {
+  const button = document.querySelector("#button");
+  button.addEventListener("click", () => {
+    alert("Oh dang, you clicked the thing!");
+  });
+});
