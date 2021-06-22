ebef7c40edaf81043cf81078885b0ae3960f7d83
andy-aguilar <andres.aguilar@flatironschool.com>
2021-06-22 15:02:13 -0400

    Add second button functionality

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 document.addEventListener("DOMContentLoaded", () => {
+
   const button = document.querySelector("#button");
+  const buttonTwo = document.querySelector("#button-2");
+
   button.addEventListener("click", () => {
     alert("Oh dang, you clicked the thing!");
   });
+  
+  buttonTwo.addEventListener("click", () => {
+    alert("Wow, you really shouldn't have clicked me");
+  });
+
 });
